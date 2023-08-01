9ccfe1b8d36850e46361e85818c390ecff260558
Billy Burch <billyburch40@gmail.com>
2023-08-01 16:30:01 -0600

    Add methods to each array

--- arrays.rb
+++ arrays.rb
@@ -1,4 +1,16 @@
-puts friends = ["Jason", "Jake", "Jerold", "Ryan"]
-puts big_ages = [16, 18, 21, 25, 35]
-puts birthdays = [1.23, 1.28, 3.18, 9.1, 12.11]
-puts booleans = [true, false, true, false]
+print friends = ["Jason", "Jake", "Jerold", "Ryan"]
+print big_ages = [16, 18, 21, 25, 35]
+print birthdays = [1.23, 1.28, 3.18, 9.1, 12.11]
+print booleans = [true, false, true, false]
+
+# the below pop method should remove and return the last element from the array assigned to the variable friends
+print friends.pop 
+
+#the below shift method should remove and return the first element from the array assigned to the variable big_ages
+print big_ages.shift
+
+#the below unshift method should add another float (12.26) to the front of the array assigned to variable birthdays
+print birthdays.unshift 12.26
+
+#the below push method should add another boolean (true) to the end of the array assigned to variable booleans
+print booleans.push true
